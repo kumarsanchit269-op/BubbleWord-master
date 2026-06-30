1c4a3939d5a919649ba44ed62d4b1caef81c36e6
Sanchit kumar <kumarsanchit269@gmail.com>
2026-06-30 15:52:02 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,8 +11,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://bubble-wordv1.vercel.app",
+        "https://bubble-wordv1-jm9bvqskm-kumarsanchit269-8153s-projects.vercel.app",
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
